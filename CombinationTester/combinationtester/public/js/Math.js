7fe4c8be310e92b8eb0dc45642de3c40fbc16538
Andrew <atanner2727@gmail.com>
2014-11-13 21:33:00 -0500

    re designed algorithm, working on getting up and running

--- CombinationTester/combinationtester/public/js/Math.js
+++ CombinationTester/combinationtester/public/js/Math.js
@@ -11,19 +11,69 @@ function Calc()
 	for(var i=0; i<=res.length - 1; i++) {
    	arr.push(parseFloat(res[i]));
 	}
-	//Loop through the n elements
-	origArray = arr;
-	//Get options
-	//then swap
-	for(var i=0; i < arr.length; i ++)
+
+
+//permute();
+var permutations = doMath(arr);
+console.log("permutations" + permutations);
+for(var i=0; i < permutations.length; i++)
+{
+	console.log("permutations i = " + permutations[i]);
+	for(var totalMax = 1; totalMax < permutations.length + 1; totalMax++)
 	{
-	Permutate(arr);
-	arr = Swap(origArray, i);
+		var sum = 0;
+		var subPerm = perm.slice(0, totalMax);
+		console.log("subPerm is:" + subPerm);
+		for(val in subPerm)
+		{
+			console.log("curval is:" + val + "sum is:" + sum);
+			sum += val;
+		}
+		console.log("total is:" + Total + " curnum is:" + sum);
+		if(sum == Total)
+		{
+			//add
+			console.log("found it" + subPerm);
+		}
 	}
 
+}
+
+// for(perm in permutations)
+// {
+// 	console.log("perm is:" + perm);
+// 	for(totalMax = 1; totalMax < arr.length + 1; totalMax++)
+// 	//for (totalMax in range(1, (arr.length) + 1))
+// 	{
+// 		var sum = 0;
+// 		var subPerm = perm.slice(0, totalMax);
+// 		console.log("subPerm is:" + subPerm);
+// 		for(val in subPerm)
+// 		{
+// 			console.log("curval is:" + val + "sum is:" + sum);
+// 			sum += val;
+// 		}
+// 		console.log("total is:" + Total + " curnum is:" + sum);
+// 		if(sum == Total)
+// 		{
+// 			//add
+// 			console.log("found it" + subPerm);
+// 		}
+// 	}
+// }
+// for perm in all_perms(numbers):
+// 	for totalMax in range(1, (len(numbers) + 1)):
+// 		sum = 0
+// 		subPerm = perm[0: totalMax]
+// 		for val in subPerm:
+// 			sum += val
+
+// 		if(sum == numA):
+// 			print subPerm
 
 
 
+//console.log("permutation math:" + doMath([5, 3, 7, 1]));
 
 console.log("before return, combs is" + combs);
 //Build the table response
@@ -46,6 +96,32 @@ for(var i =0; i < combs.length; i++)
 document.getElementById("ResultsTable").innerHTML =  returnVar;
 
 }
+
+
+function doMath(input)
+{
+	
+
+	var permArr = [],
+usedChars = [];
+function permute(input) {
+    var i, ch;
+    for (i = 0; i < input.length; i++) {
+        ch = input.splice(i, 1)[0];
+        usedChars.push(ch);
+        if (input.length == 0) {
+            permArr.push(usedChars.slice());
+        }
+        permute(input);
+        input.splice(i, 0, ch);
+        usedChars.pop();
+    }
+    return permArr
+};
+//console.log("permutation:" + permute(input));
+return permute(input);
+}
+
 
 function Swap(arr, index)
 {
